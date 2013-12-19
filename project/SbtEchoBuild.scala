b3cf638cf9ffa226605b6d6e2564716d4248d0f4
Peter Vlugter <pvlugter@gmail.com>
2013-12-19 15:35:32 +1300

    Version 0.1.0

--- project/SbtEchoBuild.scala
+++ project/SbtEchoBuild.scala
@@ -3,7 +3,7 @@ import sbt.Keys._
 import net.virtualvoid.sbt.cross.CrossPlugin
 
 object SbtEchoBuild extends Build {
-  val Version = "0.1.0-SNAPSHOT"
+  val Version = "0.1.0"
 
   lazy val sbtEcho = Project(
     id = "sbt-echo",
