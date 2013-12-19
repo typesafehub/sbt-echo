12daa43d35d8eb5ed5700d7b4ca728e44356f09b
Peter Vlugter <pvlugter@gmail.com>
2013-12-19 14:52:49 +1300

    Update to echo version 0.1.0

--- akka/src/main/scala/com/typesafe/sbt/SbtEcho.scala
+++ akka/src/main/scala/com/typesafe/sbt/SbtEcho.scala
@@ -10,7 +10,7 @@ import java.net.URI
 object SbtEcho extends Plugin {
   import echo.EchoRun._
 
-  val EchoVersion = "0.1-a6ea92738d3f13a09750e6d741dd8553f4701979"
+  val EchoVersion = "0.1.0"
   val AspectjVersion = "1.7.3"
 
   val Echo = config("echo").extend(Compile)
